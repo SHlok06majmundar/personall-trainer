c02eb847c9358acfb1084934cdaa194b0f7055d0
shlok <majmundarshlok06@gmail.com>
2025-02-02 16:30:12 +0530

    donee

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -138,7 +138,7 @@ const Navbar = ({ currentUser }) => {
         </Mobileicon>
         <NavLogo to="/">
           <Logo src={LogoImg} />
-          Fittrack
+          Personal-trainer
         </NavLogo>
 
         <MobileMenu isOpen={isOpen}>
